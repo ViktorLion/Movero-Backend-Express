5ef17a8a15d7484f15ecc5e3fd512faad31299f4
Viktor <lionvityalion@gmail.com>
2024-03-24 00:23:17 +0200

    x

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 const express = require('express');
 const { wss } = require('./utils/websocket'); // Import wss from websocket.js
 const { connectDB } = require('./utils/db');
-const { CodeBlock } = require('./models/CodeBlock') 
 const server = express(); // Create Express server
 const cors = require('cors');
 server.use(cors())
-
+const mongoose = require('mongoose');
 
 server.use('/api/code-blocks', async (req, res) => {
-  console.log('Got to /api/code-blocks')
-   try {
- 
-     const codeBlockSchema = new mongoose.Schema({
-       id: { type: String, required: true },  
-       title: { type: String, required: true },
-       code: { type: String, required: true },
-       corectCode: { type: String, required: true },
-     });
-     const model = mongoose.model('CodeBlock', codeBlockSchema)
- 
-     const codeBlocks = await model.find({}); // Fetch all code blocks
- 
-     res.status(200).json(codeBlocks);
-   } catch (error) {
-     console.error('Error fetching code blocks:', error);
-     res.status(500).json({ message: 'Internal Server Error' });
-   }
+ console.log('Got to /api/code-blocks')
+  try {
+
+    const codeBlockSchema = new mongoose.Schema({
+      id: { type: String, required: true },  
+      title: { type: String, required: true },
+      code: { type: String, required: true },
+      corectCode: { type: String, required: true },
+    });
+    const model = mongoose.model('CodeBlock', codeBlockSchema)
+
+    const codeBlocks = await model.find({}); // Fetch all code blocks
+
+    res.status(200).json(codeBlocks);
+  } catch (error) {
+    console.error('Error fetching code blocks:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+})
+
+
+server.use('/api/test', async (req, res) => {
+  console.log('test')
  })
+
 // Create Express server 
 const startServer = async () => {
   try {
@@ -43,5 +48,3 @@ const startServer = async () => {
 
 
 startServer();
-
-
