4944416c3443b8389da4d146d5be160d0bb3afa2
Viktor <lionvityalion@gmail.com>
2024-03-24 01:10:39 +0200

    Update websocket.js

--- utils/websocket.js
+++ utils/websocket.js
@@ -1,6 +1,7 @@
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 8080 });
+console.log('WebSocket server started on ws://localhost:8080');
 let lastMessage = null;
 let rooms = {};
 
