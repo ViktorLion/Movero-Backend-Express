85f669d54e27686df91002bfb5b2baa2e7aa094c
Viktor <lionvityalion@gmail.com>
2024-03-24 00:17:34 +0200

    z

--- index.js
+++ index.js
@@ -7,29 +7,26 @@ const cors = require('cors');
 server.use(cors())
 
 
-server.use('/api/code-blocks', async (req, res) => {
- console.log('Got to /api/code-blocks')
-  try {
-    const codeBlocks = await CodeBlock.find({}); // Fetch all code blocks
-
-
-    res.status(200).json(codeBlocks);
-  } catch (error) {
-    console.error('Error fetching code blocks:', error);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-})
-
-server.use('/api/test', async (req, res) => {
-  console.log('test')
- })
-
+sserver.use('/api/code-blocks', async (req, res) => {
+  console.log('Got to /api/code-blocks')
+   try {
  
-server.use('/', async (req, res) => {
-  res.send('Hello World')
-  console.log('test')
+     const codeBlockSchema = new mongoose.Schema({
+       id: { type: String, required: true },  
+       title: { type: String, required: true },
+       code: { type: String, required: true },
+       corectCode: { type: String, required: true },
+     });
+     const model = mongoose.model('CodeBlock', codeBlockSchema)
+ 
+     const codeBlocks = await model.find({}); // Fetch all code blocks
+ 
+     res.status(200).json(codeBlocks);
+   } catch (error) {
+     console.error('Error fetching code blocks:', error);
+     res.status(500).json({ message: 'Internal Server Error' });
+   }
  })
-
 // Create Express server 
 const startServer = async () => {
   try {
@@ -46,3 +43,5 @@ const startServer = async () => {
 
 
 startServer();
+
+
